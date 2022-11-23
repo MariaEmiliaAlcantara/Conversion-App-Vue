e4b911c7dde43349478a526fb8a59233837b78c3
MariaSchinoni <maria.schinoni10@gmail.com>
2022-11-23 16:25:18 -0300

    conversion component

--- src/components/Conversion.vue
+++ src/components/Conversion.vue
@@ -1,8 +1,26 @@
 <template>
   <main class="wrapper">
     <h3>Conversão Imediata</h3>
-    <p>Cotação do dólar atualizada: {{ conversion.ask }}</p>
-    <p>Insira abaixo a quantidade em reais</p>
+    <p>Cotação do dólar atualizada: R${{ conversion.ask }}</p>
+
+    <label>
+      <input
+        type="number"
+        placeholder="Quantidade em reais"
+        v-model="totalReais"
+        v-on:input="handleInputChange"
+      />
+    </label>
+
+    <div class="container-buttons">
+      <button v-on:click="handleCleanInput">Limpar</button>
+      <button v-on:click="handleCalculate">Calcular</button>
+    </div>
+
+    <p v-if="showParagraph">
+      Com R${{ totalReais.toFixed(2).replace(".", ",") }} é possível comprar
+      USD{{ totalDolars.toFixed(2).replace(".", ",") }}
+    </p>
   </main>
 </template>
 
@@ -13,7 +31,10 @@ export default {
   name: "Conversion-Vue",
   data() {
     return {
+      totalReais: "",
+      totalDolars: 0,
       conversion: {},
+      showParagraph: false,
     };
   },
   methods: {
@@ -23,19 +44,33 @@ export default {
       );
       this.conversion = response.data.USDBRL;
     },
+    handleCleanInput() {
+      this.showParagraph = false;
+      this.totalReais = "";
+    },
+    handleCalculate() {
+      this.showParagraph = true;
+      this.totalDolars = this.totalReais / this.conversion.ask;
+    },
+    handleInputChange() {
+      this.showParagraph = false;
+    },
   },
   beforeMount() {
     this.getConversion();
   },
+  watch: {},
 };
 </script>
 
 <style scoped>
 .wrapper {
+  height: 90vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: start;
+  margin-top: 4rem;
   gap: 2rem;
   padding: 2rem 0;
   color: #4a3337;
@@ -43,5 +78,29 @@ export default {
 
 .wrapper h3 {
   font-size: 2rem;
+}
+
+.wrapper input {
+  background-color: transparent;
+  color: #4a3337;
+  border: 2px solid #4a3337;
+  border-radius: 8px;
+  padding: 0 1rem;
+  height: 3rem;
+  width: 20rem;
+  font-size: 0.875rem;
+}
+
+.container-buttons {
+  display: flex;
+  gap: 0.5rem;
+}
+.container-buttons button {
+  background-color: #4a3337;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  height: 2rem;
+  width: 8rem;
 }
 </style>
