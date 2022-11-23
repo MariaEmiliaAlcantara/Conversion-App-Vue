b3e22d951960d2da6dec28f8d9b2c70c51048192
MariaSchinoni <maria.schinoni10@gmail.com>
2022-11-23 14:06:09 -0300

    init project and global styles

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+}
+a {
+  text-decoration: none;
+  color: var(--black);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Teste</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {},
+};
+</script>
+
+<style scoped></style>
